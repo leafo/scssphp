56b940c9f8373c7b2867be57d510d77f7dd3dc37
vladimmi <vladimmi@gmail.com>
2014-08-12 11:50:40 +0300

    Added stub classes to keep backward compatibility

---
+++ classmap.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Stub classes for backward compatibiility
+ */
+
+class scssc extends \Leafo\ScssPhp\Compiler {}
+
+class scss_parser extends \Leafo\ScssPhp\Parser {}
+
+class scss_formatter extends \Leafo\ScssPhp\Formatter {}
+
+class scss_formatter_nested extends \Leafo\ScssPhp\Formatter\Nested {}
+
+class scss_formatter_compressed extends \Leafo\ScssPhp\Formatter\Compressed {}
+
+class scss_formatter_crunched extends \Leafo\ScssPhp\Formatter\Crunched {}
+
+class scss_server extends \Leafo\ScssPhp\Server {}
