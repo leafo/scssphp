770eb7a85be5f0b498be755e3de2fbbca9d3e69f
vladimmi <vladimmi@gmail.com>
2014-08-12 11:57:59 +0300

    Fixes to code style

--- classmap.php
+++ classmap.php
@@ -3,17 +3,30 @@
 /**
  * Stub classes for backward compatibiility
  */
+class scssc extends \Leafo\ScssPhp\Compiler
+{
+}
 
-class scssc extends \Leafo\ScssPhp\Compiler {}
+class scss_parser extends \Leafo\ScssPhp\Parser
+{
+}
 
-class scss_parser extends \Leafo\ScssPhp\Parser {}
+class scss_formatter extends \Leafo\ScssPhp\Formatter
+{
+}
 
-class scss_formatter extends \Leafo\ScssPhp\Formatter {}
+class scss_formatter_nested extends \Leafo\ScssPhp\Formatter\Nested
+{
+}
 
-class scss_formatter_nested extends \Leafo\ScssPhp\Formatter\Nested {}
+class scss_formatter_compressed extends \Leafo\ScssPhp\Formatter\Compressed
+{
+}
 
-class scss_formatter_compressed extends \Leafo\ScssPhp\Formatter\Compressed {}
+class scss_formatter_crunched extends \Leafo\ScssPhp\Formatter\Crunched
+{
+}
 
-class scss_formatter_crunched extends \Leafo\ScssPhp\Formatter\Crunched {}
-
-class scss_server extends \Leafo\ScssPhp\Server {}
+class scss_server extends \Leafo\ScssPhp\Server
+{
+}
