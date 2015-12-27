26ead4ce91f90250b67d2ad87f22059f7167bfd0
josh <oyejorge@gmail.com>
2015-12-27 15:34:48 -0700

    check for backslash character before calling match()

--- src/Parser.php
+++ src/Parser.php
@@ -2125,7 +2125,7 @@ class Parser
             }
 
 
-            if ($this->match('\\\\\S', $m)) {
+            if ($char === '\\' && $this->match('\\\\\S', $m)) {
                 $parts[] = $m[0];
                 continue;
             }
