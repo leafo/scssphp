1a10e42404fd9bec1e906d1c209f0a5568596b67
josh <oyejorge@gmail.com>
2015-12-27 18:02:56 -0700

    more efficient value() method

--- src/Parser.php
+++ src/Parser.php
@@ -1372,6 +1372,7 @@ class Parser
                 return true;
             }
             $this->count--;
+            return false;
         }
 
 
@@ -1385,12 +1386,23 @@ class Parser
             $this->count--;
         }
 
+        // paren
+        if ($char === '(' && $this->parenValue($out)) {
+            return true;
+        }
 
-        if ($this->parenValue($out) ||
-            $this->interpolation($out) ||
-            $this->variable($out) ||
-            $this->color($out) ||
-            $this->unit($out) ||
+        if ($char === '#') {
+            if ($this->interpolation($out) || $this->color($out)) {
+                return true;
+            }
+        }
+
+        if ($char === '$' && $this->variable($out)) {
+            return true;
+        }
+
+
+        if ($this->unit($out) ||
             $this->string($out) ||
             $this->func($out) ||
             $this->progid($out)
