942c61055d53a893f79a7d34261d609b618a1ced
Anthon Pang <apang@softwaredevelopment.ca>
2017-08-22 07:20:58 -0400

    Update scss.inc.php

--- scss.inc.php
+++ scss.inc.php
@@ -3,7 +3,7 @@ if (version_compare(PHP_VERSION, '5.4') < 0) {
     throw new \Exception('scssphp requires PHP 5.4 or above');
 }
 
-if (! class_exists('scssc', false)) {
+if (! class_exists('Leafo\ScssPhp\Version', false)) {
     include_once __DIR__ . '/src/Base/Range.php';
     include_once __DIR__ . '/src/Block.php';
     include_once __DIR__ . '/src/Colors.php';
