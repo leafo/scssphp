9d21d4d720da4b928a5040055ddf2c7d73e458d0
Cerdic <cedric@yterium.com>
2019-04-15 14:59:55 +0200

    Using the cache for the compile result :   * storing the compile result and the list of the parsed files   * when getting result from a cache, check all parsed files for their timestamp. If any changed do not use the cache but compile again
    Without any cache_options set on the compiler nothing changes.
    With cache_options set, The compile() called on an unchanged BS4 compilation only takes few ms, and if any file change, only the needed parts are parsed again while the whole is re-compiled
    Allow easy on the fly framework compilation without performance struggling

--- src/Compiler.php
+++ src/Compiler.php
@@ -175,6 +175,20 @@ class Compiler
         }
     }
 
+    public function getCompileOptions()
+    {
+        $options = array(
+            'importPaths' => $this->importPaths,
+            'registeredVars' => $this->registeredVars,
+            'registeredFeatures' => $this->registeredFeatures,
+            'encoding' => $this->encoding,
+            'sourceMap' => serialize($this->sourceMap),
+            'sourceMapOptions' => $this->sourceMapOptions,
+            'formater' => $this->formatter,
+        );
+        return $options;
+    }
+
     /**
      * Compile scss
      *
@@ -187,6 +201,29 @@ class Compiler
      */
     public function compile($code, $path = null)
     {
+
+        if ($this->cache) {
+            $cache_key = ($path ? $path : "(stdin)") . ":" . md5($code);
+            $compile_options = $this->getCompileOptions();
+            $cache = $this->cache->getCache("compile", $cache_key, $compile_options);
+            if (is_array($cache)
+                and isset($cache['dependencies'])
+                and isset($cache['out']) ) {
+                // check if any dependency file changed before accepting the cache
+                foreach ($cache['dependencies'] as $file => $mtime) {
+                    if (!file_exists($file)
+                        or filemtime($file) !== $mtime) {
+                        unset($cache);
+                        break;
+                    }
+                }
+                if (isset($cache)) {
+                    return $cache['out'];
+                }
+            }
+        }
+
+
         $this->indentLevel    = -1;
         $this->extends        = [];
         $this->extendsMap     = [];
@@ -240,6 +277,14 @@ class Compiler
             }
 
             $out .= sprintf('/*# sourceMappingURL=%s */', $sourceMapUrl);
+        }
+
+        if ($this->cache and isset($cache_key) and isset($compile_options)) {
+            $v = array(
+                'dependencies' => $this->getParsedFiles(),
+                'out' => &$out,
+            );
+            $this->cache->setCache("compile", $cache_key, $v, $compile_options);
         }
 
         return $out;
