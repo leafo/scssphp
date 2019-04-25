3f9f2056cea7b9771b7ed68ebbcf0908accfdb1b
Cerdic <cedric@yterium.com>
2019-04-25 15:56:00 +0200

    Adding test for BS4 and Foundation 6 frameworks This is an update of https://github.com/leafo/scssphp/pull/462 which is outdated

---
+++ tests/FrameworkTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * SCSSPHP
+ *
+ * @copyright 2012-2015 Leaf Corcoran
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * @link http://leafo.github.io/scssphp
+ */
+
+namespace Leafo\ScssPhp\Tests;
+
+use Leafo\ScssPhp\Compiler;
+
+class FrameworkTest extends \PHPUnit_Framework_TestCase
+{
+    protected static $frameworks = [
+        [
+            "frameworkVerion" => "twbs/bootstrap4.3",
+            "inputdirectory" => "../vendor/twbs/bootstrap/scss/",
+            "inputfiles" => "bootstrap.scss",
+        ],
+        [
+            "frameworkVerion" => "zurb/foundation6.5",
+            "inputdirectory" => "../vendor/zurb/foundation/assets/",
+            "inputfiles" => "foundation.scss",
+        ]
+    ];
+
+    private $saveDir;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->scss = new Compiler();
+        $this->saveDir = getcwd();
+
+        chdir(__DIR__);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        chdir($this->saveDir);
+    }
+
+    /**
+     * @dataProvider frameworkProvider
+     */
+    public function testFramework($frameworkVerion, $inputdirectory, $inputfiles)
+    {
+        $this->scss->addImportPath($inputdirectory);
+
+        $input = file_get_contents($inputdirectory.$inputfiles);
+        
+        //Test if no exeption are raised for the given framwork
+        $e = null;
+        try {
+            $this->scss->compile($input, $inputfiles);
+        } catch (Exception $e) {
+            // test fail
+        }
+
+        $this->assertNull($e);
+    }
+
+    public function frameworkProvider(){
+        return self::$frameworks;
+    }
+}
