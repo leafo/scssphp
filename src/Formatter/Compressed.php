43eb9f0b7d499f6ca8d65a6bd031f779f67b1b9e
Cerdic <cedric@yterium.com>
2019-05-13 16:44:33 +0200

    Fix #598: striping space around relationshop operators in format crunched or compressed

--- src/Formatter/Compressed.php
+++ src/Formatter/Compressed.php
@@ -59,4 +59,18 @@ class Compressed extends Formatter
             $this->write($this->break);
         }
     }
+
+    /**
+     * Output block selectors
+     *
+     * @param \Leafo\ScssPhp\Formatter\OutputBlock $block
+     */
+    protected function blockSelectors(OutputBlock $block)
+    {
+        $inner = $this->indentStr();
+
+        $this->write($inner
+            . implode($this->tagSeparator, str_replace(array(' > ', ' + ', ' ~ '), array('>', '+', '~'), $block->selectors))
+            . $this->open . $this->break);
+    }
 }
