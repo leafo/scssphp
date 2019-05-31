986957e227fe0b19774d479ea250f859a739776a
Anthon Pang <apang@softwaredevelopment.ca>
2019-05-31 13:40:17 -0400

    Remove tests that no longer fail

--- tests/FailingTest.php
+++ tests/FailingTest.php
@@ -62,56 +62,6 @@ class FailingTest extends \PHPUnit_Framework_TestCase
     {
         // @codingStandardsIgnoreStart
         return array(
-            array(
-                '#67 - weird @extend behavior', <<<'END_OF_SCSS'
-.nav-bar {
-    background: #eee;
-    > .item {
-        margin: 0 10px;
-    }
-}
-
-
-header ul {
-    @extend .nav-bar;
-    > li {
-        @extend .item;
-    }
-}
-END_OF_SCSS
-                , <<<END_OF_EXPECTED
-.nav-bar, header ul {
-  background: #eee; }
-  .nav-bar > .item, header ul > .item, header ul > li {
-    margin: 0 10px; }
-END_OF_EXPECTED
-            ),
-            array(
-                '#368 - self in selector', <<<'END_OF_SCSS'
-.test {
-    &:last-child:not(+ &:first-child) {
-      padding-left: 10px;
-    }
-}
-END_OF_SCSS
-                , <<<END_OF_EXPECTED
-.test:last-child:not(+ .test:first-child) {
-  padding-left: 10px; }
-END_OF_EXPECTED
-            ),
-            array(
-                '#371 - nested self selector', <<<'END_OF_SCSS'
-ul, ol {
-    & & {
-      display: block;
-    }
-  }
-END_OF_SCSS
-                , <<<END_OF_EXPECTED
-ul ul, ol ul, ul ol, ol ol {
-  display: block; }
-END_OF_EXPECTED
-            ),
 /*************************************************************
             array(
                 '', <<<'END_OF_SCSS'
